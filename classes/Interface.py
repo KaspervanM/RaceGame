3a5592c5d76e133192d3f038d6500cc2e87f3c6d
Kasper van Maasdam <kaspervanM@gmail.com>
2020-05-21 12:55:04 +0200

    Update Interface.py

--- classes/Interface.py
+++ classes/Interface.py
@@ -1,8 +1,8 @@
 class Interface():
-	__slots__ = ['rotations', 'input', 'key', 'viewline', 'xc', 'yc', 'LevelLayer_width', 'LevelLayer_height', 'pixels', 'score', 'carWidth', 'speed', 'modelindex', 'model']
-	def __init__(self, rotations, input, key, viewline, xc, yc, LevelLayer_width, LevelLayer_height, pixels, score, carWidth, speed, modelindex, model):
+	__slots__ = ['rotations', 'inputkeys', 'key', 'viewline', 'xc', 'yc', 'LevelLayer_width', 'LevelLayer_height', 'pixels', 'score', 'carWidth', 'speed', 'modelindex', 'model']
+	def __init__(self, rotations, inputkeys, key, viewline, xc, yc, LevelLayer_width, LevelLayer_height, pixels, score, carWidth, speed, modelindex, model):
 		self.rotations = rotations  # 0
-		self.input = input  # [0,0,0,0]
+		self.inputkeys = inputkeys  # [0,0,0,0]
 		self.key = key  # [0,0,0,0]
 		self.viewline = viewline  # []
 		self.xc = xc  # 0
@@ -17,25 +17,25 @@ class Interface():
 		self.model = model
 
 	def set_code(self):
-		input = self.input
+		inputkeys = self.inputkeys
 		key = self.key
-		if (input[0] == 1):  # left
+		if (inputkeys[0] == 1):  # left
 			self.key[0] = 1
 		elif (key[0] == 1):
 			self.key[0] = 0
 
-		if (input[1] == 1):  # right
+		if (inputkeys[1] == 1):  # right
 			self.key[1] = 1
 		elif (key[1] == 1):
 			self.key[1] = 0
 
-		if (input[2] == 1):  # up
+		if (inputkeys[2] == 1):  # up
 			if key[3] != 1: self.key[2] = 1
 		elif (key[2] == 1):
 			if key[3] != -2: self.key[2] = -1
 			else: self.key[2] = 0
 
-		if (input[3] == 1):  # down
+		if (inputkeys[3] == 1):  # down
 			if key[2] != 1: self.key[3] = 1
 		elif (key[3] == 1):
 			if key[2] != -1: self.key[3] = -2
