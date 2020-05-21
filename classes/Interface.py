37a20b78cf472c35dc3c4991e870234403a6a6c4
Restyled.io <commits@restyled.io>
2020-05-21 10:59:18 +0000

    Restyled by black

--- classes/Interface.py
+++ classes/Interface.py
@@ -1,8 +1,38 @@
-class Interface():
-    __slots__ = ['rotations', 'inputkeys', 'key', 'viewline', 'xc', 'yc', 'LevelLayer_width',
-                 'LevelLayer_height', 'pixels', 'score', 'carWidth', 'speed', 'modelindex', 'model']
+class Interface:
+    __slots__ = [
+        "rotations",
+        "inputkeys",
+        "key",
+        "viewline",
+        "xc",
+        "yc",
+        "LevelLayer_width",
+        "LevelLayer_height",
+        "pixels",
+        "score",
+        "carWidth",
+        "speed",
+        "modelindex",
+        "model",
+    ]
 
-    def __init__(self, rotations, inputkeys, key, viewline, xc, yc, LevelLayer_width, LevelLayer_height, pixels, score, carWidth, speed, modelindex, model):
+    def __init__(
+        self,
+        rotations,
+        inputkeys,
+        key,
+        viewline,
+        xc,
+        yc,
+        LevelLayer_width,
+        LevelLayer_height,
+        pixels,
+        score,
+        carWidth,
+        speed,
+        modelindex,
+        model,
+    ):
         self.rotations = rotations  # 0
         self.inputkeys = inputkeys  # [0,0,0,0]
         self.key = key  # [0,0,0,0]
@@ -21,29 +51,29 @@ class Interface():
     def set_code(self):
         inputkeys = self.inputkeys
         key = self.key
-        if (inputkeys[0] == 1):  # left
+        if inputkeys[0] == 1:  # left
             self.key[0] = 1
-        elif (key[0] == 1):
+        elif key[0] == 1:
             self.key[0] = 0
 
-        if (inputkeys[1] == 1):  # right
+        if inputkeys[1] == 1:  # right
             self.key[1] = 1
-        elif (key[1] == 1):
+        elif key[1] == 1:
             self.key[1] = 0
 
-        if (inputkeys[2] == 1):  # up
+        if inputkeys[2] == 1:  # up
             if key[3] != 1:
                 self.key[2] = 1
-        elif (key[2] == 1):
+        elif key[2] == 1:
             if key[3] != -2:
                 self.key[2] = -1
             else:
                 self.key[2] = 0
 
-        if (inputkeys[3] == 1):  # down
+        if inputkeys[3] == 1:  # down
             if key[2] != 1:
                 self.key[3] = 1
-        elif (key[3] == 1):
+        elif key[3] == 1:
             if key[2] != -1:
                 self.key[3] = -2
             else:
