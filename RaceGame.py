40a82ff5748997320d493bdeceb6285be57723dc
Kasper van Maasdam <kaspervanM@gmail.com>
2020-05-21 12:15:16 +0200

    Update RaceGame.py
    Some improvements/

--- RaceGame.py
+++ RaceGame.py
@@ -1,16 +1,16 @@
 import pyglet
 from keras.models import load_model
+from math import ceil
 from os import mkdir
 from os.path import exists
 from shutil import rmtree
-from collections import OrderedDict
-from classes.Interface import Interface
 from classes.Cars import Car
-from classes.NN import *
+from classes.NN import NNev, NNHuman, NNLongest, generate_random_NNev_model, gen_mutant
+from numpy.random import shuffle, randint, seed
 import sys
 import time
 from ast import literal_eval
-seed(777)
+seed(10)
 def get_platform():
     platforms = {
         'linux1' : 'Linux',
@@ -28,23 +28,26 @@ currdir = sys.path[0]
 car_image = pyglet.image.load(currdir+fs+'resources'+fs+'car.png')
 Level = pyglet.image.load(currdir+fs+'resources'+fs+'Level.png').get_texture()
 LevelLayer = pyglet.image.load(currdir+fs+'resources'+fs+'LevelLayerWscore2.png').get_texture()
-global b, inf, car
+
 b = True
+show_cars = True
 inf = False
+showmodel = False
 
 Human = 0
 
-nModels = 4 * (1-Human)
-nCarsPerModel = 4 * (1-Human)
+nModels = 3 * (1-Human)
+nCarsPerModel = 2 * (1-Human)
 nCars = nModels * nCarsPerModel + Human
 car = []
 
-global generation, save_yet, models, model_scores
+
 generation = 0
 start_time = int(time.time())
-save_yet = 0
 models = {}
 model_scores = []
+
+modelpath = currdir+fs+'resources'+fs+'models'+fs
 
 def update(dt = 0):
 	dt = 0.03
@@ -54,9 +57,9 @@ def update(dt = 0):
 
 def getArray(image):
 	rawimage = image.get_image_data()
-	format = 'I'
-	pitch = rawimage.width * len(format)
-	return rawimage.get_data(format, pitch)
+	colourformat = 'I'
+	pitch = rawimage.width * len(colourformat)
+	return rawimage.get_data(colourformat, pitch)
 
 def center_image(image):
 	"""Sets an image's anchor point to its center"""
@@ -65,7 +68,7 @@ def center_image(image):
 
 def save_model(tup):
 	kompos = tup[0].find(',')
-	path = currdir+fs+'resources'+fs+'models'+fs+f'type{tup[0][:kompos]}'
+	path = modelpath+f'type{tup[0][:kompos]}'
 	if not exists(path): mkdir(path)
 	tup[1].save(path+fs+f'model{tup[0][kompos+1:]}.h5')
 
@@ -86,7 +89,7 @@ def set_car():
 				if f"{i},{x}" in models:
 					car.append(Car(rotation = -90, scale=(game_window.height / 600 + game_window.width / 800) / 6, img=car_image, x=450, y=80))
 
-					car[index].nViewlines = 10 * (0 if Human and i == 0 else 1)  # (i+1-Human) * 4
+					car[index].nViewlines = 8
 					car[index].IFLocal.modelindex = [i, x]
 					car[index].IFLocal.model = models[f"{i},{x}"]
 					car[index].IFLocal.LevelLayer_width = LevelLayer.width
@@ -101,12 +104,19 @@ def empty_room():
 	save_yet += 1
 	tot_time = int(time.time()) - start_time
 	print(f'gen {generation} completed, {tot_time} sec passed in total')
-	top = (nCars-Human)//3
+	top = ceil((nCars-Human)/2)
 	best = [(k, model_scores[k]) for k in sorted(model_scores, key=model_scores.get, reverse=True)]
-	most = (len(best)*3)//4
-	if all(s[1] == best[0][1] for s in best[:most]):
-		best = best[:most]
-		shuffle(best)
+
+	indexes = [0]
+	app = indexes.append
+	for n in range(1, len(best)):
+		if best[n-1] != best[n]:
+			app(n)
+	for x in range(len(indexes)-1):
+		copy = best[indexes[x]:indexes[x+1]]
+		shuffle(copy)
+		best[indexes[x]:indexes[x+1]] = copy
+
 	best = best[:top]
 	print('best: ', best)
 	index = Human
@@ -119,38 +129,37 @@ def empty_room():
 						best_model = models[best[bestindex][0]]
 						del models[f"{i},{x}"]
 
-						I = int(best[bestindex][0][0])
-						if randint(0, nCars*best[index % top][1]**1.5) == 0:
+						mutation_rate = 1/((25+best[bestindex][1])/25)
+						nexti = int(best[bestindex][0][0])
+						if randint(0, int(nCars*(best[index % top][1]+1)**1.5)) == 0:
 							print('Model mutated')
-							mutated_model = generate_random_NNev_model(best_model, 1/((10+best[bestindex][1])/10))
-							print(f"I before: {I}")
-							I += 1
+							mutated_model = gen_mutant(generate_random_NNev_model(best_model, mutation_rate), mutation_rate)
+							print(f"nexti before: {nexti}")
+							nexti += 1
 							for p in range(50):
 								if not (f"{p},0" in models or f"{p},1" in models or f"{p},0" in models2 or f"{p},1" in models2):
-									I = p
+									nexti = p
 									break
-							print(f"I after: {I}")
+							print(f"nexti after: {nexti}")
 						else:
-							mutated_model = gen_mutant(best_model, 1/((10+best[bestindex][1])/10))
+							mutated_model = gen_mutant(best_model, mutation_rate)
 						nextx = int(best[bestindex][0][2])+1
 						for p in range(100):
-							if not (f"{I},{p}" in models or f"{I},{p}" in models2):
+							if not (f"{nexti},{p}" in models or f"{nexti},{p}" in models2):
 								nextx = p
 								break
-						models2[f"{I},{nextx}"] = mutated_model
+						models2[f"{nexti},{nextx}"] = mutated_model
 						index += 1
 	models.update(models2)
-	if save_yet == 10:
-		path = currdir+fs+'resources'+fs+'models'
-		if exists(path): rmtree(path, ignore_errors=True)
-		while exists(path): # check if it exists
+	if generation % 10 == 0:
+		if exists(modelpath): rmtree(modelpath, ignore_errors=True)
+		while exists(modelpath): # check if it exists
 			pass
-		mkdir(path)
-		with open(currdir+fs+'resources'+fs+'models'+fs+'gen.log', 'w') as f:
+		mkdir(modelpath)
+		with open(modelpath+'gen.log', 'w') as f:
 			f.write(f'[{generation}, {tot_time}]')
-		for key in models:
-			save_model((key, models[key]))
-		save_yet = 0
+		for item in models.items():
+			save_model((item[0], item[1]))
 	model_scores = {}
 	set_car()
 
@@ -170,7 +179,7 @@ def collision(index):
 		car[index] = None
 
 	print("boem", end='\r')
-	if all((elem == None) for elem in car):
+	if all((elem is None) for elem in car):
 		empty_room()
 
 center_image(car_image)
@@ -184,29 +193,28 @@ while all(inp != ans for ans in ['yes', 'no', 'y', 'n', 'Yes', 'No', 'Y', 'N']):
 if any(inp == ans for ans in ['no', 'n', 'No', 'N']):
 	print('ok, no then')
 	model_scores = {}
-	for i in range(nModels):
+	for modnum in range(nModels):
 		model = generate_random_NNev_model()
-		print(i)
+		print(modnum)
 		for x in range(nCarsPerModel):
-			models[f"{i},{x}"] = gen_mutant(gen_mutant(model, 1.), 1.)
+			models[f"{modnum},{x}"] = gen_mutant(gen_mutant(model, 1.), 1.)
 else:
-	path = currdir+fs+'resources'+fs+'models'+fs
-	with open(path+'gen.log', 'r') as f:
-		data = literal_eval(f.read())
+	with open(modelpath+'gen.log', 'r') as logfile:
+		data = literal_eval(logfile.read())
 		generation = data[0]
 		start_time = int(time.time())-data[1]
-	for i in range(nModels):
-		if exists(f"{path}type{i}"):
+	for modnum in range(50):
+		if exists(f"{modelpath}type{modnum}"):
 			for x in range(100):
-				if exists(f"{path}type{i}{fs}model{x}.h5"):
-					models[f"{i},{x}"] = load_model(f"{path}type{i}{fs}model{x}.h5")
+				if exists(f"{modelpath}type{modnum}{fs}model{x}.h5"):
+					models[f"{modnum},{x}"] = load_model(f"{modelpath}type{modnum}{fs}model{x}.h5")
 	model_scores = {}
 set_car()
 update()
 
 @game_window.event
 def on_key_press(key, modifiers):
-	global b, inf
+	global b, inf, showmodel, show_cars
 	if(key == pyglet.window.key.LEFT):
 		car[0].NN.output[0] = 1
 	elif(key == pyglet.window.key.RIGHT):
@@ -216,9 +224,13 @@ def on_key_press(key, modifiers):
 	elif(key == pyglet.window.key.DOWN):
 		car[0].NN.output[3] = 1
 	elif(key == pyglet.window.key.B):
-		b = False if b else True
+		b = not bool(b)
 	elif(key == pyglet.window.key.I):
-		inf = False if inf else True
+		inf = not bool(inf)
+	elif(key == pyglet.window.key.S):
+		showmodel = not bool(showmodel)
+	elif(key == pyglet.window.key.C):
+		show_cars = not bool(show_cars)
 
 @game_window.event
 def on_key_release(key, modifiers):
@@ -239,14 +251,16 @@ def on_draw():
 	else: LevelLayer.blit(0,0)
 	pyglet.text.Label('FPS: {:4.2f}'.format(pyglet.clock.get_fps()),
 	 font_name='Times New Roman', font_size=10, x=Level.width - 100, y=Level.height-40).draw()
-	for i in range(len(car)):
-		if car[i]:
-			car[i].draw()
-			car[i].draw_info(10,game_window.height - 20*i - 20, not inf)
-			pyglet.text.Label('{:d}, {:d}'.format(car[i].IFLocal.modelindex[0], car[i].IFLocal.modelindex[1]),
-			 font_name='Times New Roman', font_size=10, x=car[i].x, y=car[i].y).draw()
-			if (car[i].check_collision(car[i].hitbox) or (not (Human and i == 0) and car[i].unusedframes > 100)):
-				collision(i)
+	for count, elem in enumerate(car):
+		if elem:
+			if show_cars:
+				elem.draw()
+				elem.draw_info(10,game_window.height - 20*count - 20, not inf)
+				if showmodel and car[0]: car[0].show_model()
+				pyglet.text.Label('{:d}, {:d}'.format(elem.IFLocal.modelindex[0], elem.IFLocal.modelindex[1]),
+				 font_name='Times New Roman', font_size=10, x=elem.x, y=elem.y).draw()
+			if (elem.check_collision(elem.hitbox) or (not (Human and count == 0) and elem.unusedframes > 100)):
+				collision(count)
 pyglet.clock.schedule_interval(update, 1/120.0)
 
 if __name__ == '__main__':
